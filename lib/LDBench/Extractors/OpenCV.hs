ce9ed87057214dff77733a21b9a9e378e95c2e5e
Eric Martin Christiansen <ericmartinchristiansen@gmail.com>
2014-03-10 14:00:39 -0700

    extractor stuff

--- lib/LDBench/Extractors/OpenCV.hs
+++ lib/LDBench/Extractors/OpenCV.hs
@@ -2,8 +2,16 @@ module LDBench.Extractors.OpenCV where
 
 import qualified Data.Vector
 import qualified Data.Text.Lazy
+import Data.Bits.Lens
+import Control.Exception
+import Data.Word
+import Control.Lens
+import Data.Array.Repa hiding (map)
+import Data.Maybe
 
 import OpenCVThrift.OpenCV
+import OpenCVThrift.OpenCV.Core
+import OpenCVThrift.OpenCV.Core.MatUtil
 import qualified OpenCVThrift.OpenCV.Features2D.Features2D
 import OpenCVThrift.OpenCV.Features2D
 
@@ -11,6 +19,50 @@ import LDBench.Extractor
 import LDBench.Convert
 
 data BRISK = BRISK
+
+toRepa :: Mat -> OpenCVComputation (Array U DIM3 Double)
+toRepa mat = do
+  matUnpacked <- convert1 unpack mat 
+  let
+    rows = fromIntegral $ fromJust $ f_MatUnpacked_rows matUnpacked
+    cols = fromIntegral $ fromJust $ f_MatUnpacked_cols matUnpacked
+    channels = fromIntegral $ fromJust $ f_MatUnpacked_channels matUnpacked
+    data' = Data.Vector.toList $ fromJust $ f_MatUnpacked_data matUnpacked
+  return $ fromListUnboxed (Z :. rows :. cols :. channels) data'
+
+meld :: [[Double]] -> [Bool] -> [Maybe [Double]]
+meld rows mask = 
+  assert (length rows == length (filter id mask)) $
+  undefined
+
+fromExtractorResponse :: ExtractorResponse -> OpenCVComputation [Maybe [Double]]
+fromExtractorResponse 
+  (ExtractorResponse (Just descriptorMat) (Just keyPointMask)) = do
+     descriptorRepa <- toRepa descriptorMat
+     let
+       getRow :: Int -> [Double]
+       getRow i = toList $ slice descriptorRepa (Z :. i :. All :. (0 :: Int)) 
+       [numRows, _, numChannels] = listOfShape $ extent descriptorRepa 
+       rows = map getRow [0 .. numRows - 1]
+     return $ 
+       assert (numChannels == 0) $ 
+       meld rows (Data.Vector.toList keyPointMask) 
+
+doubleToBitstring :: Double -> [Bool]
+doubleToBitstring double =
+  assert (double >= 0) $
+  assert (double < 256) $
+  let rounded = round double :: Word8 in
+  assert (fromIntegral rounded == double) $
+  [ rounded ^. bitAt 0
+  , rounded ^. bitAt 1 
+  , rounded ^. bitAt 2 
+  , rounded ^. bitAt 3 
+  , rounded ^. bitAt 4 
+  , rounded ^. bitAt 5 
+  , rounded ^. bitAt 6 
+  , rounded ^. bitAt 7 
+  ]
 
 instance Extractor BRISK [Bool] where
   extract BRISK image keyPoints = do
@@ -20,7 +72,8 @@ instance Extractor BRISK [Bool] where
       (Data.Text.Lazy.pack "BRISK")
       imageMat
       (Data.Vector.fromList keyPoints)
-    {-return $ do-}
+    return $ do
+      index <- [0 .. Data.Vector.length keyPointMask - 1]
+      undefined
 
-    undefined
 
