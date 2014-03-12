7cfef938e8fa89cf74c93136f0aeedcca49badb8
Eric Martin Christiansen <ericmartinchristiansen@gmail.com>
2014-03-12 11:27:04 -0700

    readHomography in progress

--- lib/LDBench/Experiments/WideBaseline/Util.hs
+++ lib/LDBench/Experiments/WideBaseline/Util.hs
@@ -1,6 +1,14 @@
 module LDBench.Experiments.WideBaseline.Util where
 
+import Control.Monad
+import Text.Regex
+import Text.RawString.QQ
+
 import LDBench.Experiments.WideBaseline.Homography
 
 readHomography :: FilePath -> IO Homography
-readHomography = undefined
+readHomography filePath = do
+  lines' <- liftM lines $ readFile filePath
+  let
+    floatingPointRegex = mkRegex [r|([-+]?[0-9]*\.?[0-9]+.)|]
+  undefined
